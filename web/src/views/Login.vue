d2a6e6886aabf2ba7fa3989ceda106eaeafd7458
vemann <vemann@ttu.ee>
2019-02-21 09:48:16 +0200

    frontend

---
+++ web/src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="login">
+        <form @submit="CheckForm" method="post">
+            <label>Email</label>
+            <input type="email" name="email" v-model="email" />
+            <br/>
+            <label>Password</label>
+            <input type="password" name="password" v-model="password">
+            <br/>
+            <input type="submit" name="submit" value="Submit" />
+        </form>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'login',
+        methods: {
+            CheckForm: function (e) {
+                if (this.email && this.password) {
+                    return true;
+                }
+
+                this.errors = [];
+
+                if (!this.email) {
+                    this.errors.push('Email required.');
+                }
+                if (!this.password) {
+                    this.errors.push('Password required.');
+                }
+
+                e.preventDefault();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    * {
+        margin-top: 10px;
+    }
+
+</style>
