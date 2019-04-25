bf66066093713afa8cee5368facda4124a8df1d1
Erik Puidet <erikpuidet@gmail.com>
2019-04-25 10:55:23 +0300

    Fixing tests

--- src/test/java/app/fitness/IntegrationTests.java
+++ src/test/java/app/fitness/IntegrationTests.java
@@ -17,9 +17,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.testng.annotations.BeforeTest;
 
 
+import static app.fitness.util.createValidWorkout;
 import static app.fitness.util.createWorkoutPlan;
 import static app.fitness.util.obtainAccessToken;
 import static junit.framework.TestCase.assertEquals;
@@ -43,74 +43,46 @@ public class IntegrationTests {
     private static String token;
 
 
-    @Test
+    @Before
     public void loginTest() throws Exception {
         token = obtainAccessToken("user2", "user", mvc);
-        System.out.println("TOKEN: " + token);
+        System.out.println("BEFORE: " + token);
         assertNotNull(token);
     }
 
 
     @Test
-    public void WorkoutServiceGetByUserName() throws Exception {
-        String token = obtainAccessToken("user2", "user", mvc);
-        Workout workout = new Workout();
-        workout.setWorkoutName("Test workout");
-        workout.setUserName("user2");
-        // Check that user has no workouts inserted
-        ResultActions result = mvc.perform(get("/api/workouts/user2")
-                .param("access_token", token))
-                .andExpect(status().isOk());
-        String json = result.andReturn().getResponse().getContentAsString();
+    public void WorkoutServiceGetByUserNameEmpty() throws Exception {
+        String json = getRequest("/api/workouts/user2");
         ObjectMapper mapper = new ObjectMapper();
         Workout[] workouts = mapper.readValue(json, Workout[].class);
         assertArrayEquals(workouts, new Workout[0]);
-
-        // Post new workout to user.
-        String postJson = mapper.writeValueAsString(workout);
-        mvc.perform(post("/api/workouts")
-            .contentType(MediaType.APPLICATION_JSON)
-            .param("access_token", token)
-            .content(postJson))
-            .andExpect(status().isOk());
-        // Get all workouts and check that the new one is there
-        result = mvc.perform(get("/api/workouts/user2")
-                .param("access_token", token))
-                .andExpect(status().isOk());
-        json = result.andReturn().getResponse().getContentAsString();
-        workouts = mapper.readValue(json, Workout[].class);
-        assertEquals(workouts[0].getWorkoutName(),"Test workout");
     }
 
     @Test
-    public void PostAndGetNewWorkoutPlan() throws Exception {
-        String token = obtainAccessToken("user2", "user", mvc);
-        WorkoutPlan plan = createWorkoutPlan("Test plan");
-
-
-        // Check that user has no plans inserted
-        ResultActions result = mvc.perform(get("/api/plan")
-                .param("access_token", token))
-                .andExpect(status().isOk());
-        String json = result.andReturn().getResponse().getContentAsString();
+    public void WorkoutServicePost() throws Exception {
+        Workout workout = createValidWorkout("user2", "Upper");
         ObjectMapper mapper = new ObjectMapper();
-        WorkoutPlan[] plans = mapper.readValue(json, WorkoutPlan[].class);
-        int initialLength = plans.length;
-        System.out.println("INITIAL LENGTH: " + initialLength);
-        // Post new plan.
+        String postJson = mapper.writeValueAsString(workout);
+        ResultActions result = postRequest(postJson, "/api/workouts");
+        String json = result.andReturn().getResponse().getContentAsString();
+        Workout resultWorkout = mapper.readValue(json, Workout.class);
+        assertEquals(resultWorkout.getId(), Long.valueOf(1));
+    }
+
+
+    @Test
+    public void GetWorkoutPlanController() throws Exception {
+        String json = getRequest("/api/plan");
+        ObjectMapper mapper = new ObjectMapper();
+        int initialLength = mapper.readValue(json, WorkoutPlan[].class).length;
+        WorkoutPlan plan = createWorkoutPlan("Test plan");
         String postJson = mapper.writeValueAsString(plan);
-        System.out.println("postJason");
-        mvc.perform(post("/api/plan")
-                .contentType(MediaType.APPLICATION_JSON)
-                .param("access_token", token)
-                .content(postJson))
-                .andExpect(status().isOk());
+
+        postRequest(postJson, "/api/plan");
         // Get all plans and check that the new one is there
-        result = mvc.perform(get("/api/plan")
-                .param("access_token", token))
-                .andExpect(status().isOk());
-        json = result.andReturn().getResponse().getContentAsString();
-        plans = mapper.readValue(json, WorkoutPlan[].class);
+        json = getRequest("/api/plan");
+        WorkoutPlan[] plans = mapper.readValue(json, WorkoutPlan[].class);
 
         // Check that a workoutPlan with name "Test plan" is in the array.
         boolean planNameInArray = false;
@@ -119,9 +91,23 @@ public class IntegrationTests {
                 planNameInArray = true;
             }
         }
-        // Check that the new a new plan was added and 1 more object is in database
         assertEquals(initialLength + 1, plans.length);
-        // Check that there is and
         assertTrue(planNameInArray);
+    }
+
+    private ResultActions postRequest(String postJson, String url) throws Exception {
+        return mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("access_token", token)
+                .content(postJson))
+                .andExpect(status().isOk());
+    }
+
+
+    private String getRequest(String url) throws Exception {
+        ResultActions result = mvc.perform(get(url)
+                .param("access_token", token))
+                .andExpect(status().isOk());
+        return result.andReturn().getResponse().getContentAsString();
     }
 }
